9ad2dd3140759e6d2a1c235aff1fb03c378ee111
daohoangson123 <108396570+daohoangson123@users.noreply.github.com>
2024-12-23 20:53:03 +0700

    first commit - fe only

---
+++ fe-gumishop/src/app/ui/font.ts
@@ -0,0 +1,8 @@
+import { Jost, Lusitana } from 'next/font/google';
+
+export const jost = Jost({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
